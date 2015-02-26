9878c6c2c5568b83e6b679d097e5a2bc5f3a7da4
Eemeli Kelokorpi <eemelikelokorpi@eemelis-mbp.ylead.fi>
2015-02-26 17:24:55 +0200

    Update

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-## Panda App
+# Panda Editor
+
+### Powerful code editor for Panda.js game development
+
+With Panda Editor, you can speed up your development and use Toolkit features right from your editor.
+
+## Install
+
+- Coming soon
+
+## Features
+
+- Split your code into classes and modules
+- Code more efficiently with keyboard shortcuts
+- Build your project straight from the editor
+- Change layout with different themes
+- See changes instantly in your game _(coming)_
+
 
--- build.js
+++ build.js
@@ -4,19 +4,15 @@ var NwBuilder = require('node-webkit-builder');
 var nw = new NwBuilder({
     files: './src/**/**',
     platforms: ['osx64'],
-    appName: 'Panda App',
+    appName: 'Panda Editor',
     // version: '0.11.5',
     macIcns: './icons/panda.icns',
     buildType: 'versioned'
 });
 
 nw.build(function(err) {
-	if (err) {
-		console.log(err);
-	}
-	else {
-		console.log('Done');
-	}
+	if (err) console.log(err);
+	else console.log('Done');
 });
 
 // Copy /usr/local/bin/node to
