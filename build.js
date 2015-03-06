bc6da0984ea31c304c7eb9e767bb743645211fd9
Eemeli Kelokorpi <eemelikelokorpi@eemelis-mbp.ylead.fi>
2015-03-06 13:09:16 +0200

    Update

--- build.js
+++ build.js
@@ -1,28 +1,79 @@
+var info = require('./src/package.json');
 var NwBuilder = require('node-webkit-builder');
-var platform = process.argv[2] || 'osx';
+var platforms = process.argv[2] ? [process.argv[2]] : ['osx', 'win'];
+
+var package = {
+    run: function() {
+        var platform = platforms.pop();
+        if (!platform) {
+            console.log('All done');
+            return;
+        }
+
+        this[platform]();
+    },
+
+    win: function() {
+        console.log('Packaging win...');
+        var file_system = require('fs');
+        var archiver = require('archiver');
+
+        var output = file_system.createWriteStream('release/panda.js-editor-' + info.version + '.zip');
+        var archive = archiver('zip');
+
+        output.on('close', function () {
+            console.log('Done');
+            package.run();
+        });
+
+        archive.on('error', function(err){
+            throw err;
+        });
+
+        archive.pipe(output);
+        archive.bulk([
+            { expand: true, cwd: './build/Panda Editor/win64', src: ['**'], dest: 'Panda Editor'}
+        ]);
+        archive.finalize();
+    },
+
+    osx: function() {
+        console.log('Packaging osx...');
+        var appdmg = require('appdmg');
+        var ee = appdmg({ source: 'dmg.json', target: 'release/panda.js-editor-' + info.version + '.dmg' });
+
+        ee.on('finish', function () {
+            console.log('Done');
+            package.run();
+        });
+    }
+};
 
 var config = {
     files: './src/**/**',
     appName: 'Panda Editor',
+    platforms: ['osx64', 'win64'],
+    macIcns: './res/icons/panda.icns',
+    winIco: './res/icons/panda.ico',
     // version: '0.11.5',
-    buildDir: './build',
-    buildType: 'versioned'
+    // buildType: 'versioned',
+    buildDir: './build'
 };
 
-if (platform === 'osx') {
-    config.platforms = ['osx32'];
-    config.macIcns = './res/icons/panda.icns';
+if (platforms.indexOf('win') === -1) {
+    config.platforms = ['osx64'];
+    delete config.winIco;
 }
-else {
-    platform = 'win';
-    config.platforms = ['win32'];
-    config.winIco = './res/icons/panda.ico';
+if (platforms.indexOf('osx') === -1) {
+    config.platforms = ['win64'];
+    delete config.macIcns;
 }
 
 var nw = new NwBuilder(config);
 
-console.log('Building for ' + platform + '...');
+console.log('Building for ' + platforms.join(', ') + '...');
+
 nw.build(function(err) {
 	if (err) console.log(err);
-	else console.log('Done');
+	else package.run();
 });
